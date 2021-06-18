6b52949b482b1296114d9af74e83ce1a65a6f341
dbdimitrov <d.boykoff@gmail.com>
2021-06-18 10:05:41 +0200

    update liana, re-run cbmc as test

--- analysis/cbmc_analyse/cbmc_run.R
+++ analysis/cbmc_analyse/cbmc_run.R
@@ -1,25 +1,99 @@
 # Run on local
 
-# Load and Save Data
-# cbmcdata <- SeuratData::LoadData("cbmc") %>%
-#     Seurat::NormalizeData() %>%
-#     Seurat::FindVariableFeatures()
-#
-# cbmcdata@meta.data %<>%
-#     mutate(celltype = as.factor(rna_annotations)) %>%
-#     # remove + as it breaks Squidpy
-#     mutate(rna_annotations = str_replace_all(rna_annotations, "[+]", "")) %>%
-#     mutate(rna_annotations = str_replace(rna_annotations, "/", " ")) %>%
-#     mutate(rna_annotations = as.factor(rna_annotations))
-# cbmcdata <- subset(cbmcdata, cells = rownames(cbmcdata@meta.data))
-# cbmcdata <- SetIdent(cbmcdata, value = cbmcdata@meta.data$rna_annotations)
-# saveRDS(cbmcdata, "input/cbmc_seurat.rds")
-cbmcdata <- readRDS("input/cbmc_seurat.rds")
+# Load and and format Data
+SeuratData::InstallData("cbmc")
 
-# Get Full CCC Resources
-# omni_resources <- compile_ligrec()
-# saveRDS(omni_resources, "input/omni_resources.rds")
-omni_resources <- readRDS("input/omni_resources.rds")
+cbmcdata <- SeuratData::LoadData("cbmc") %>%
+    Seurat::NormalizeData() %>%
+    Seurat::FindVariableFeatures()
+
+cbmcdata@meta.data %<>%
+    mutate(celltype = as.factor(rna_annotations)) %>%
+    # remove + as it breaks Squidpy
+    mutate(rna_annotations = str_replace_all(rna_annotations, "[+]", "")) %>%
+    mutate(rna_annotations = str_replace(rna_annotations, "/", " ")) %>%
+    mutate(rna_annotations = as.factor(rna_annotations))
+cbmcdata <- subset(cbmcdata, cells = rownames(cbmcdata@meta.data))
+cbmcdata <- SetIdent(cbmcdata, value = cbmcdata@meta.data$rna_annotations)
+saveRDS(cbmcdata, "data/input/cbmc_seurat.rds")
+cbmcdata <- readRDS("data/input/cbmc_seurat.rds")
+
+
+# Call liana
+cmbc_cc <- liana_wrap(seurat_object = cbmcdata,
+           resource = "all",
+           cellchat.params = list(
+                   nboot = 1000,
+                   thresh = 1,
+                   assay = "RNA"
+                   ),
+           squidpy.params = list(
+               cluster_key="seurat_annotations",
+               n_perms=10000,
+               threshold=0.01,
+               seed=as.integer(1004)
+           ))
+
+
+
+default_args <- list(
+    "cellchat" = cellchat.params %<>%
+        `%||%`(list(
+            nboot = 100,
+            exclude_anns = NULL,
+            thresh = 1,
+            assay = assay,
+            .normalize = FALSE,
+            .do_parallel = FALSE,
+            .raw_use = TRUE
+        )),
+
+    'connectome' = connectome.params %<>%
+        `%||%`(list(
+            min.cells.per.ident = 1,
+            p.values = TRUE,
+            calculate.DOR = FALSE,
+            assay = assay,
+            .format = TRUE,
+            .spatial = FALSE
+        )),
+
+    'italk' = italk.params %<>%
+        `%||%`(list(
+            assay = assay,
+            .format = TRUE,
+            .DE = TRUE
+        )),
+
+    'natmi' = natmi.params %<>%
+        `%||%`(list(
+            expr_file = "em.csv",
+            meta_file = "metadata.csv",
+            output_dir = "NATMI_test",
+            assay = "RNA",
+            num_cor = 4,
+            .format = TRUE,
+            .write_data = TRUE,
+            .seed = 1004,
+            .natmi_path = NULL
+        )),
+
+    'sca' = sca.params %<>%
+        `%||%`(list(
+            assay = assay,
+            .format = TRUE,
+            s.score = 0,
+            logFC = log2(1.5))),
+
+    'squidpy' = squidpy.params %<>%
+        `%||%`(list(
+            cluster_key="seurat_annotations",
+            n_perms=1000,
+            threshold=0.01,
+            seed=as.integer(1004)
+        ))
+)
+
 
 
 # 1. Squidpy -------------------------------------------------------------------
