3daee7e036ea101a316ea95c312dbf2f891990d3
dbdimitrov <d.boykoff@gmail.com>
2021-09-16 19:01:49 +0200

    reformat cite-seq

--- analysis/test/test_citeseq.R
+++ analysis/test/test_citeseq.R
@@ -4,6 +4,20 @@ require(liana)
 require(tidyverse)
 require(magrittr)
 
+
+# make this thing into a function
+# filter out any interactions that don't include the ADT-RNA intersect to save time.
+# perform it with 3 datasets + 3 intervals (0, 0.05, 0.1, 0.2)
+# plot together
+
+
+
+# We Run Kendal Correlation, which supposedly uses tau-b scores (i.e. accounts for ties)
+# Only 7 receptors match to OmniPath LRs, if I consider also ligands it would be a higher number,
+# but very similar, but it would need to be separately, i.e. by source and target, so it becomes confusing
+# and in my opinion redundant - ligand and receptors overlap also.
+
+
 # Load matrix and convert to Seurat object ----
 # https://support.10xgenomics.com/single-cell-gene-expression/datasets/3.1.0/5k_pbmc_protein_v3
 mats <- Seurat::Read10X("data/input/10x_citeseq/5k_pbmc_protein_v3_filtered_feature_bc_matrix/filtered_feature_bc_matrix/")
@@ -22,6 +36,14 @@ seurat_object %<>%
     FindNeighbors(reduction = "pca") %>%
     FindClusters(resolution = 0.4, verbose = TRUE)
 
+# Normalize ADT
+seurat_object <- NormalizeData(seurat_object,
+                               assay = "ADT",
+                               normalization.method = "CLR")
+# seurat_object <- ScaleData(seurat_object, assay = "ADT")
+
+
+
 # issue with squipy nums - need to fix
 clust.anns <- str_glue("c{levels(seurat_object)}")
 names(clust.anns) <- levels(seurat_object)
@@ -36,7 +58,9 @@ saveRDS(seurat_object, "data/input/cmbc_seurat_test.RDS")
 
 # RUN LIANA on cbmc test data ----
 liana_res <- liana_wrap(seurat_object,
-                        squidpy.params=list(cluster_key = "seurat_clusters"))
+                        squidpy.params=list(cluster_key = "seurat_clusters"),
+                        expr_prop = 0.1,
+                        )
 saveRDS(liana_res, "data/output/test_citeseq.RDS")
 
 
@@ -48,7 +72,7 @@ liana_res <- readRDS("data/output/test_citeseq.RDS")
 # Get ADT stats ----
 # convert to singlecell object
 sce <- SingleCellExperiment::SingleCellExperiment(
-    assays=list(counts = GetAssayData(seurat_object, assay = "ADT")),
+    assays=list(counts = GetAssayData(seurat_object, assay = "ADT", slot = "data")),
     colData=DataFrame(label=seurat_object@meta.data$seurat_clusters)
     )
 op_resource <- select_resource("OmniPath")[[1]]
@@ -154,12 +178,11 @@ prop_corr <- adt_corr %>%
     mutate(prop_mlog10p = -log10(p.value)) %>%
     select(name, prop_pval = p.value, prop_estimate = estimate)
 
-corr_join <- mean_corr %>%
+corr_format <- mean_corr %>%
+    # join
     left_join(scale_corr) %>%
     # left_join(prop_corr) %>%
-    dplyr::rename("method" = name)
-
-corr_format <- corr_join %>%
+    dplyr::rename("method" = name) %>%
     pivot_longer(-method,
                  names_to = "corr_type") %>%
     separate(corr_type, into = c("metric", "stat")) %>%
@@ -171,18 +194,25 @@ corr_format <- corr_join %>%
 
 # plot
 corr_format %>%
+    # remove Mean/Median ranks
+    filter(!(method %in% c("median_rank", "mean_rank"))) %>%
     # rename methods
     mutate(method = str_to_title(method)) %>%
     mutate(method = gsub("\\..*","",method)) %>%
-    filter(!(method %in% c("median_rank", "mean_rank"))) %>%
-    mutate(estimate = -1*estimate) %>% # reverse negative estimates\
+    mutate(estimate = -1*estimate) %>% # reverse negative estimates
     # rename metrics
     mutate(metric = if_else(metric=="scale", "Cluster-specific Mean", metric)) %>%
     mutate(metric = if_else(metric=="mean", "Mean", metric)) %>%
     ggplot(aes(x = metric, y = estimate, colour = method, shape = significant)) +
     geom_point(size = 6, position = position_dodge(w = 0.05)) +
     xlab("Expression Type") +
-    ylab("Kendal Correlation Coefficients") +
+    ylab("Kendal's tau Correlation Coefficients") +
     theme_minimal(base_size = 24) +
     labs(colour = "Method", shape="Significant")
+
+
+# ROC according to Proportion thresholds ----
+hist(adt_means_entity[,-1] %>% as.matrix() %>% as.numeric(), breaks=100)
+# normalize and cut anything below 0, i.e. assume that its noise
+
 
