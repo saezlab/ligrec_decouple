e5d252837384a7ec631b194125c8ff8b2d67367a
dbdimitrov <d.boykoff@gmail.com>
2021-10-06 18:24:41 +0200

    change to pipe

--- analysis/test/citeseq_roc.R
+++ analysis/test/citeseq_roc.R
@@ -10,16 +10,164 @@ require(pheatmap)
 source("analysis/citeseq/citeseq_src.R")
 
 
+op_resource <- select_resource("OmniPath")[[1]]
+murine_resource <- select_resource("OmniPath")[[1]] %>%
+    convert_to_murine()
+arbitrary_thresh = 1.282 # alpha = 0.1
+citeseq_dir <- "data/input/citeseq/"
+
+
 # test
 seurat_object <- readRDS("data/input/citeseq/10k_pbmcs/10k_pbmcs_seurat.RDS")
 liana_res <- readRDS("data/input/citeseq/10k_pbmcs/10k_pbmcs-liana_res-0.1.RDS")
-op_resource <- select_resource("OmniPath")[[1]]# %>%
-    # convert_to_murine()
 cluster_key = "seurat_clusters"
-organism="human"
-pp_decoupler <- readRDS("~/Downloads/php_result.rds")
-arbitrary_thresh = 1.282 # alpha = 0.1
 
+pp_decoupler <- readRDS("~/Downloads/php_result.rds")
+
+test <- generate_specificity_roc(seurat_object = seurat_object,
+                                 liana_res = liana_res,
+                                 op_resource = op_resource,
+                                 cluster_key = "seurat_clusters",
+                                 organism="human",
+                                 arbitrary_thresh = arbitrary_thresh)
+
+
+### RUN all sets ----
+roc_tibble <- list.files(citeseq_dir) %>%
+    # Get ROCS
+    map(function(subdir){
+        # Run LIANA /w mouse specific
+        if(stringr::str_detect(subdir, pattern = "spleen")){
+            load_adt_roc(subdir = subdir,
+                         dir = citeseq_dir,
+                         op_resource = murine_resource,
+                         organism = "mouse",
+                         cluster_key = "seurat_clusters",
+                         arbitrary_thresh = arbitrary_thresh,
+                         sobj_pattern = "_seurat.RDS",
+                         liana_pattern = "liana_res-0.1.RDS"
+                         )
+        } else { # human
+            load_adt_roc(subdir = subdir,
+                         dir = citeseq_dir,
+                         op_resource = op_resource,
+                         organism = "human",
+                         cluster_key = "seurat_clusters",
+                         arbitrary_thresh = arbitrary_thresh,
+                         sobj_pattern = "_seurat.RDS",
+                         liana_pattern = "liana_res-0.1.RDS"
+            )
+        }
+    }) %>% setNames(list.files(citeseq_dir)) %>%
+    enframe() %>%
+    unnest(value) %>%
+    rename(dataset = name)
+
+
+roc_tibble
+
+
+
+
+
+
+
+## COPY-PASTED - needs to be amended to work with both generate_specificity_roc and liana_adt_whatever
+
+#' ADT-LR correlation pipeline Helper/Loader Function
+#' @param
+#' @inheritParams wrap_adt_corr
+#'
+#' @returns a tibble with summarized ADT-LR correlations
+#'
+#' @details Simply loads the needed seurat and liana objects and calls the
+#' `wrap_adt_corr` function/pipeline.
+#' `seurat_object_path` = seurat_object.RDS path
+#' `liana_res_path` = liana_res.RDS path
+load_adt_roc <- function(subdir = subdir,
+                         dir,
+                         sobj_pattern = "_seurat.RDS",
+                         liana_pattern,
+                         op_resource,
+                         cluster_key,
+                         organism,
+                         arbitrary_thresh){
+
+    message(str_glue("Calculating ROC summary for {subdir}"))
+
+    seurat_object_path <- list.subfiles(subdir = subdir,
+                                        dir = citeseq_dir,
+                                        pattern = sobj_pattern)
+
+
+    liana_res_path <- list.subfiles(subdir = subdir,
+                                    dir = citeseq_dir,
+                                    pattern = liana_pattern)
+
+    generate_specificity_roc(seurat_object = readRDS(seurat_object_path),
+                             liana_res = readRDS(liana_res_path),
+                             op_resource = op_resource,
+                             cluster_key = cluster_key,
+                             organism=organism,
+                             arbitrary_thresh = arbitrary_thresh)
+}
+
+
+
+#' PLOT TO BE FUNCTION
+specificity_summary(adt_rank_roc){
+    # ROC DF
+    df_roc <- adt_rank_roc %>%
+        # +dataset_name
+        dplyr::select(method_name, roc) %>%
+        unnest(roc)
+
+    # PRROC DF
+    df_prc <- adt_rank_roc %>%
+        # +dataset_name
+        dplyr::select(method_name, prc) %>%
+        unnest(prc)
+
+    # ROC plot
+    ggplot(df_roc, aes(x = 1-specificity,
+                       y = sensitivity,
+                       colour = .data$method_name)) +
+        geom_line() +
+        geom_abline(intercept = 0, slope = 1, linetype = "dashed") +
+        xlab("FPR (1-specificity)") +
+        ylab("TPR (sensitivity)")
+
+    # ROC heatmap
+    roc_heat <- df_roc %>%
+        as.data.frame() %>%
+        dplyr::select(method_name, auc) %>%
+        distinct() %>%
+        column_to_rownames("method_name") %>%
+        pheatmap(.,
+                 cluster_rows = FALSE,
+                 cluster_cols = FALSE,
+                 treeheight_col = 0,
+                 treeheight_row = 0,
+                 display_numbers = TRUE,
+                 silent = TRUE)
+    roc_heat
+
+    # PRROC heatmap
+    prc_heat <- df_prc %>%
+        as.data.frame() %>%
+        dplyr::select(method_name, auc) %>%
+        distinct() %>%
+        column_to_rownames("method_name") %>%
+        pheatmap(.,
+                 cluster_rows = FALSE,
+                 cluster_cols = FALSE,
+                 treeheight_col = 0,
+                 treeheight_row = 0,
+                 display_numbers = TRUE,
+                 silent = TRUE)
+    prc_heat
+
+}
 
 
 # get adt_ranks DF (need to change it to ADT-SCORE)
@@ -112,18 +260,7 @@ prepare_for_roc = function(df, arbitrary_thresh){
                                            adt_scale <= arbitrary_thresh ~ 0)) %>%
         mutate(response = factor(response, levels = c(1, 0))) %>%
         dplyr::select(source.target.entity, method_name,
-                      adt_scale, predictor, response) %>%
-        group_by(method_name) %>%
-        group_nest(.key = "adt_rank") %>%
-        # Calculate ROC
-        mutate(roc = .data$adt_rank %>%
-                   map(function(df) calc_curve(df))) %>%
-        # Calculate PRROC
-        mutate(prc = .data$adt_rank %>%
-                   map(function(df) calc_curve(df,
-                                               curve = "PR",
-                                               downsampling = TRUE,
-                                               times = 100)))
+                      adt_scale, predictor, response)
 }
 
 
@@ -313,3 +450,40 @@ get_rank_adt <- function(seurat_object,
     return(liana_adt)
 }
 
+
+
+
+#' Generate ROC
+generate_specificity_roc <- function(seurat_object,
+                                     liana_res,
+                                     op_resource,
+                                     cluster_key,
+                                     organism,
+                                     ...){
+    # get adt_ranks DF (need to change it to ADT-SCORE)
+    adt_rank_results <- get_rank_adt(seurat_object = seurat_object,
+                                     liana_res = liana_res,
+                                     op_resource = op_resource,
+                                     cluster_key = cluster_key,
+                                     organism = organism)
+
+    # return(hist(adt_rank_results$adt_scale))
+
+
+    # To appropriate format (prepare_for_roc) and do ROC
+    adt_rank_roc <- adt_rank_results %>%
+        prepare_for_roc(arbitrary_thresh = arbitrary_thresh) %>%
+        group_by(method_name) %>%
+        group_nest(.key = "adt_rank") %>%
+        # Calculate ROC
+        mutate(roc = .data$adt_rank %>%
+                   map(function(df) calc_curve(df))) %>%
+        # Calculate PRROC
+        mutate(prc = .data$adt_rank %>%
+                   map(function(df) calc_curve(df,
+                                               curve = "PR",
+                                               downsampling = TRUE,
+                                               times = 100)))
+
+    return(adt_rank_roc)
+}
