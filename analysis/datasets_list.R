bd141fb84162ab8172f8270992ce255baa58de28
dbdimitrov <d.boykoff@gmail.com>
2021-10-07 14:59:56 +0200

    pipeline somewhat refractored

---
+++ analysis/datasets_list.R
@@ -0,0 +1,17 @@
+#
+require(tidyverse)
+require(Seurat)
+require(SeuratDisk)
+
+# https://atlas.fredhutch.org/nygc/multimodal-pbmc/
+# hfile <- Connect("data/input/seurat_wnn_data/multi.h5seurat")
+
+
+seurat_object <- LoadH5Seurat("multi.h5seurat",
+                              assays="counts", reductions = FALSE,
+                              graphs = FALSE, neighbors = FALSE)
+
+saveRDS(seurat_object@meta.data, "metadata.rds")
+
+# KotliarovPBMCData
+# MairPBMCData
