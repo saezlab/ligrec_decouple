d617cfa9b79360cbf507ffc457304dd2023fed03
dbdimitrov <d.boykoff@gmail.com>
2021-11-03 10:12:05 +0100

    appropriate file path

--- analysis/comparison/brca_prep.sh
+++ analysis/comparison/brca_prep.sh
@@ -19,6 +19,6 @@ arraylength=${#subtypes[@]}
 # Loop over brca_types and first comm arg is path_to_project
 for (( i=0; i<${arraylength}; i++ ));
 do
- echo "Submitted: Rscript ./comp_brca_prep.R $1 ${subtypes[$i]}"
- Rscript ./comp_brca_prep.R $1 ${subtypes[$i]}
+ echo "Submitted: Rscript analysis/comparison/comp_brca_prep.R $1 ${subtypes[$i]}"
+ Rscript analysis/comparison/comp_brca_prep.R $1 ${subtypes[$i]}
 done
