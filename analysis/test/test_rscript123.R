f35afb179a24f2db4630c72e633a4fa73eaa6f89
dbdimitrov <d.boykoff@gmail.com>
2021-11-03 09:45:06 +0100

    add basic test for clust

--- analysis/test/test_rscript123.R
+++ analysis/test/test_rscript123.R
@@ -7,10 +7,10 @@ liana_path <- system.file(package = "liana")
 testdata <-
     readRDS(file.path(liana_path , "testdata", "input", "testdata.rds"))
 
-liana_res <- liana_wrap(seurat_object, method = c('call_natmi',  'call_connectome',
-                                                  'logfc', 'cellchat',
-                                                  'call_sca', 'squidpy',
-                                                  "cytotalk"))
+liana_res <- liana_wrap(testdata, method = c('call_natmi',  'call_connectome',
+                                             'logfc', 'cellchat',
+                                             'call_sca', 'squidpy',
+                                             "cytotalk"))
 liana_res
 
 saveRDS(liana_res, "data/output/test123.RDS")
