b6effcf7f2156d42b98ece43bdf4e77723fa5c92
dbdimitrov <d.boykoff@gmail.com>
2021-11-03 10:08:16 +0100

    amend .sh to clust

--- analysis/comparison/brca_prep.sh
+++ analysis/comparison/brca_prep.sh
@@ -1,4 +1,15 @@
 #!/bin/bash
+#SBATCH -p single
+#SBATCH -N 1
+#SBATCH --time=71:00:00
+#SBATCH --mem=200000
+#SBATCH --job-name="brca_run"
+#SBATCH --output=brca_run.out
+#SBATCH --mail-user=daniel.dimitrov@uni-heidelberg.de
+#SBATCH --mail-type=ALL
+#SBATCH --requeue
+#SBATCH --chdir /net/data.isilon/ag-saez/bq_ddimitrov/Repos/ligrec_decouple
+
 ## declare an array variable
 declare -a subtypes=("ER" "TNBC" "HER2")
 
