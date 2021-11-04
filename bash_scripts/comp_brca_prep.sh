d7d9ca2fdcf6ffbfe9e449bbf20f973fbdf5ea36
dbdimitrov <d.boykoff@gmail.com>
2021-11-04 13:04:13 +0100

    add bash scripts

---
+++ bash_scripts/comp_brca_prep.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#SBATCH -p single
+#SBATCH -N 1
+#SBATCH --time=71:00:00
+#SBATCH --mem=200000
+#SBATCH --job-name="brca_run"
+#SBATCH --output=out/brca_run.out
+#SBATCH --mail-user=daniel.dimitrov@uni-heidelberg.de
+#SBATCH --mail-type=ALL
+#SBATCH --requeue
+#SBATCH --chdir /net/data.isilon/ag-saez/bq_ddimitrov/Repos/ligrec_decouple
+
+# $SLURM_SUBMIT_DIR project directory, $1 cancer subtype  - subtypes=("ER" "TNBC" "HER2")
+ echo "Submitted: Rscript analysis/comparison/comp_brca_prep.R $SLURM_SUBMIT_DIR ${subtypes[$i]}"
+ Rscript analysis/comparison/comp_brca_prep.R $SLURM_SUBMIT_DIR $1
