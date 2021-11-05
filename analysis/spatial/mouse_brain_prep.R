5629c2ed30e2eb9097ab65f6a1795d587cccd0de
dbdimitrov <d.boykoff@gmail.com>
2021-11-05 21:38:05 +0100

    prep brain

--- analysis/spatial/mouse_brain_prep.R
+++ analysis/spatial/mouse_brain_prep.R
@@ -120,26 +120,5 @@ topic_profile_plts <- SPOTlight::dot_plot_profiles_fun(
 topic_profile_plts[[2]]
 
 
-#
-# # III) Run LIANA ----
-# gc()
-# cortex_sc <- readRDS(file.path(brain_dir, "allen_cortex_dwn.rds"))
-# murine_resource <- readRDS("data/input/murine_omnipath.RDS")
-#
-# liana_res <- liana_wrap(cortex_sc,
-#                         cellchat.params=list(organism="mouse"),
-#                         resource = "custom",
-#                         external_resource = murine_resource,
-#                         assay = "SCT",
-#                         expr_prop=0.1,
-#                         squidpy.params = list(cluster_key = "subclass"))
-#
-#
-# # squidpy sets gene names to upper (in the processing), revert this to title (i.e. murine)
-# # set all others to title just in case
-# liana_res %<>% map(function(res) res %>%
-#                        mutate_at(.vars = c("ligand", "receptor"), str_to_title))
-# saveRDS(liana_res, "data/input/spatial/brain_cortex/brain_liana_results.RDS")
-
 
 
