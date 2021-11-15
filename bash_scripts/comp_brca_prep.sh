28c9d08ff5671ca4893864dc43e896193e33fb80
dbdimitrov <d.boykoff@gmail.com>
2021-11-15 11:40:51 +0100

    pass arg to sbatch

--- bash_scripts/comp_brca_prep.sh
+++ bash_scripts/comp_brca_prep.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
+sbatch <<EOT
+#!/bin/bash
+
 #SBATCH -p single
 #SBATCH -N 1
 #SBATCH --time=71:00:00
 #SBATCH --mem=100000
-#SBATCH --job-name="brca_run"
-#SBATCH --output=out/brca_run.out
+#SBATCH --job-name=$1"brca_run"
+#SBATCH --output="out/"$1"brca_run.out"
+#SBATCH --e="out/"$1"brca_run.err"
 #SBATCH --mail-user=daniel.dimitrov@uni-heidelberg.de
 #SBATCH --mail-type=ALL
 #SBATCH --requeue
@@ -13,3 +17,8 @@
 # project directory, $1 cancer subtype  - subtypes=("ER" "TNBC" "HER2")
 echo "Submitted: Rscript analysis/comparison/comp_brca_prep.R $1"
 Rscript analysis/comparison/comp_brca_prep.R $1
+
+hostname
+exit 0
+
+EOT
