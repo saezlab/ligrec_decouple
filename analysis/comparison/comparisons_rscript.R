db8c54f21b55e874a8be2412da9b0f9f2ffcb294
dbdimitrov <d.boykoff@gmail.com>
2021-11-17 18:55:07 +0100

    prepped for all comps

--- analysis/comparison/comparisons_rscript.R
+++ analysis/comparison/comparisons_rscript.R
@@ -9,7 +9,8 @@ args <- commandArgs(trailingOnly=TRUE)
 
 # Get Job name
 job_name <- args[[2]] # e.g. crc
-# seurat_path <- args[[3]] # e.g. data/input/comparison/crc_data/crc_korean_form.rds
+seurat_path <- args[[3]] # e.g. data/input/comparison/crc_data/crc_korean_form.rds
+
 # Script name
 message(str_glue("Running: {args[[1]]} for {job_name}"))
 
