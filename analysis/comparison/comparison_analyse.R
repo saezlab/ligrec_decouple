fd471b1117ced23baae080055a6b90a89097f21a
dbdimitrov <d.boykoff@gmail.com>
2021-11-22 16:53:08 +0100

    comp analysis

--- analysis/comparison/comparison_analyse.R
+++ analysis/comparison/comparison_analyse.R
@@ -22,8 +22,8 @@ source("src/plot_utils.R")
 args <- commandArgs(trailingOnly=TRUE)
 
 # Get Job name
-input_filepath <- args[[2]] # e.g. "data/output/comparison_out/panc8_liana_res.RDS"
-output_filepath <- args[[3]] # e.g. panc8_out
+input_filepath <- args[[3]] # e.g. "data/output/comparison_out/panc8_liana_res.RDS"
+output_filepath <- args[[2]]  # e.g. panc8_out
 
 # Summarize comparisons
 comparison_summary(input_filepath = input_filepath,
