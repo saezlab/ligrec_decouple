a68dbe3181ed4dd11b1954a382232ca1eae56384
dbdimitrov <d.boykoff@gmail.com>
2021-11-22 21:27:39 +0100

    comp analysis

--- analysis/comparison/comparison_summarise.R
+++ analysis/comparison/comparison_summarise.R
@@ -22,7 +22,7 @@ source("src/plot_utils.R")
 args <- commandArgs(trailingOnly=TRUE)
 
 # Get Job name
-output_filepath <- args[[2]]  # e.g. panc8_out (+ job name)
+output_filepath <- args[[2]]  # e.g. "panc8_specs_n" (+ job name)
 input_filepath <- args[[3]] # e.g. "data/output/comparison_out/panc8_liana_res.RDS"
 setting <- args[[4]]
 
@@ -43,12 +43,11 @@ if(setting=="specs_frac"){
 }
 
 
-
 # Summarize comparisons
 comparison_summary(input_filepath = input_filepath,
                    output_filepath = output_filepath,
                    resource = "OmniPath",
-                   top_x = top_fun,
+                   top_x = top_x,
                    top_fun = top_fun,
                    .score_specs = .score_specs,
                    cap_value_freq = 1,
