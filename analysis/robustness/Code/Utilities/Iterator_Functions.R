cec784cfc8a171716e2ee990e548b2cab8dea7a2
LeoBurmedi <49957645+LeoBurmedi@users.noreply.github.com>
2021-12-04 09:09:09 +0100

    Since the same get top ranks function is used for all robustness tests now, the formatting of those top ranks is now a common utility

--- analysis/robustness/Code/Utilities/Iterator_Functions.R
+++ analysis/robustness/Code/Utilities/Iterator_Functions.R
@@ -281,3 +281,35 @@
   }
 }
 
+# format_top_ranks()
+{
+  #' Add an two columns, one contains unique Ligand-Receptor combinations, the
+  #' other unique Source-Target-Ligand-Receptor combinations.
+  #'
+  #' @param top_ranks A tibble that has CCIs in it that have the source, target,
+  #' ligand and receptor listed.
+  #'
+  #' @return A tibble like the input with a column of unique tags per LR and
+  #'  STLR.
+
+  format_top_ranks <- function(top_ranks) {
+    # Format the top_ranks data frame for future processing steps.
+    top_ranks <- top_ranks %>%
+      unite("LR_Pair",
+            c(ligand, receptor),
+            remove = FALSE,
+            sep = "_") %>%
+      relocate("LR_Pair", .after = last_col()) %>%
+      unite("LR_ID",
+            c(source, target, ligand, receptor),
+            remove = FALSE,
+            sep = "_") %>%
+      relocate("LR_ID", .after = last_col())
+
+
+    return(top_ranks)
+
+  }
+
+
+}
