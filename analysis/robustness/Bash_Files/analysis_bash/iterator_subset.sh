a4c70ee150f834c26ac9a6551d8d72ba9dd8091b
LeoBurmedi <49957645+LeoBurmedi@users.noreply.github.com>
2021-12-06 08:26:38 +0100

    Changed email, filepaths, arguments reduced to the relevant ones

--- analysis/robustness/Bash_Files/analysis_bash/iterator_subset.sh
+++ analysis/robustness/Bash_Files/analysis_bash/iterator_subset.sh
@@ -5,11 +5,11 @@
 #SBATCH --mem=20000
 #SBATCH --job-name="subset_iterator"
 #SBATCH --output=subset_iterator.out
-#SBATCH --mail-user=p.burmedi@stud.uni-heidelberg.de
+#SBATCH --mail-user=[insert email]
 #SBATCH --mail-type=ALL
 #SBATCH --requeue
-#SBATCH --chdir /net/data.isilon/ag-saez/bq_pburmedi/repos/ligrec_robustness
+#SBATCH --chdir /net/data.isilon/ag-saez/[insert filepath]/repos/ligrec_decouple
 
 
-# Add arguments to pass to Run_Iterator. 1 = reshuffle_or_subset, 2 = top_n, 3 = job_id
-/net/data.isilon/ag-saez/bq_pburmedi/SOFTWARE/miniconda3/envs/liana_env/bin/Rscript Code/Analysis_Scripts/Clusters_Run_Iterator.R "subset" 500 $SLURM_JOBID  
+# Add arguments to pass to Run_Iterator. 1 = reshuffle_or_subset, 2 = job_id
+/net/data.isilon/ag-saez/[filepath to liana_env and Rscript] analysis/robustness/Code/Analysis_Scripts/Clusters_Run_Iterator.R "subset" $SLURM_JOBID  
