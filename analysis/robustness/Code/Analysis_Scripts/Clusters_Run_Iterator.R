c5d55e0147dae7ea4cee9409cd05e22aac72e73d
LeoBurmedi <49957645+LeoBurmedi@users.noreply.github.com>
2021-12-06 08:25:36 +0100

    Updated code sourcing for new filepaths and the portions of code that have been deleted, updated to take only the args from the bash file that are necessary for the basic application

--- analysis/robustness/Code/Analysis_Scripts/Clusters_Run_Iterator.R
+++ analysis/robustness/Code/Analysis_Scripts/Clusters_Run_Iterator.R
@@ -11,40 +11,38 @@
   # In this segment, we set up all the required infrastructure to run the
   # wrapper. The default parameters for the wrapper are set in
   # CR_Iterator.R, but custom parameters can be set in the function call.
-  
+
   # Load required Packages
   require(tidyverse)
   require(Seurat)
   require(liana)
   require(lubridate)
-  
-  
+
+
   # Define the functions needed to perform our analysis
-  
+
   # Define wrap_cluster_Iterator(), a function that performs the core analysis
-  source("Code/Cluster_Reshuffling/CR_Iterator.R")
-  # Define Iterator helper functions that work with the top-ranked CCI's
-  source("Code/Cluster_Reshuffling/Iterator_Top_Ranks.R")
-  # Define Iterator helpers that work at a meta level, saving results and so on 
-  source("Code/Cluster_Reshuffling/Iterator_Meta_and_Saves.R")
+  source("analysis/robustness/Code/Cluster_Reshuffling/CR_Iterator.R")
+  # Define Iterator helpers that work at a meta level, saving results and so on
+  source("analysis/robustness/Code/Cluster_Reshuffling/Iterator_Meta_and_Saves.R")
 
-  
+
   # Define functions for generating the many reshuffled cluster annotations
-  source("Code/Cluster_Reshuffling/CR_Shuffler_Functions.R")
+  source("analysis/robustness/Code/Cluster_Reshuffling/CR_Shuffler_Functions.R")
   # Define functions for subsetting cluster annotations, instead of reshuffling
-  source("Code/Cluster_Reshuffling/CR_Subsetter_Functions.R")
+  source("analysis/robustness/Code/Cluster_Reshuffling/CR_Subsetter_Functions.R")
   # Define functions for iterating LIANA on all the reshuffled clusters
-  source("Code/Cluster_Reshuffling/CR_LIANA_Functions.R")
+  source("analysis/robustness/Code/Cluster_Reshuffling/CR_LIANA_Functions.R")
 
 
-  # Define utility functions that help the iterator run 
-  source("Code/Utilities/Iterator_Functions.R")
+  # Define utility functions that help the iterator run
+  source("analysis/robustness/Code/Utilities/Iterator_Functions.R")
   # Define utiltiy functions for user-facing console outputs and plotting
-  source("Code/Utilities/User_Outputs_and_Plots.R")
+  source("analysis/robustness/Code/Utilities/User_Outputs_and_Plots.R")
   # Define common functions that help with NATMI parameters
-  source("Code/Utilities/LIANA_Utilities.R")
-  
-  
+  source("analysis/robustness/Code/Utilities/LIANA_Utilities.R")
+
+
 }
 
 
@@ -55,47 +53,47 @@
 # We get SLURM input arguments, including job_id to mark NATMI files with.
 args = commandArgs(TRUE)
 
-reshuffle_or_subset = as.character(args[1])
-top_n               = as.numeric(args[2])
-job_id              = as.numeric(args[3])
+reshuffle_or_subset <- as.character(args[1])
+job_id              <- as.numeric(args[2])
 
 
 
-# First we load testdata from the data folder. 
+# First we load testdata from the data folder.
 # We also give a label (testdata_type, choose "seurat_pbmc" or "liana_test")
-testdata_type <- "seurat_pbmc"  
+testdata_type <- "seurat_pbmc"
 testdata      <- extract_Testdata(testdata_type = testdata_type)
 
 
 # Double-check the Inputs
 {
-  important_args <- 
-    c("testdata_type", "reshuffle_or_subset", "top_n", "job_id") 
-  
+  important_args <-
+    c("testdata_type",
+      "reshuffle_or_subset",
+      "job_id")
+
   map(important_args, function(arg) {
     c(arg, get(arg), typeof(get(arg)))
-    
-  }) %>% 
+
+  }) %>%
     transpose %>%
     set_names(c("name", "value", "data_type")) %>%
-    as_tibble() %>% 
+    as_tibble() %>%
     unnest(c("name", "value", "data_type")) %>%
     print()
-  
+
   cat("\n\nTestdata: \n")
   print(testdata)
 }
 
 
 # We run the wrapper with test settings
-robustness_cluster <- 
+robustness_cluster <-
   wrap_cluster_Iterator(testdata      = testdata,
                         testdata_type = testdata_type,
-                        
+
                         reshuffle_or_subset = reshuffle_or_subset,
-                        top_n               = top_n,
                         NATMI_tag           = job_id)
-  
+
 
 # Extra info for console output
 print(robustness_cluster)
--- analysis/robustness/Code/Analysis_Scripts/Resources_Run_Iterator.R
+++ analysis/robustness/Code/Analysis_Scripts/Resources_Run_Iterator.R
@@ -8,58 +8,56 @@
 #------------------------------------------------------------------------------#
 # 1. Setup ---------------------------------------------------------------------
 {
-  # In this segment, we set up all the required infrastructure to run the 
-  # wrapper. The default parameters for the wrapper are set in 
-  # RD_Robustness_Iterator.R, but custom parameters can be set in the function 
-  # call.
-  
-  # Load required Packages
-  require(tidyverse)
-  require(Seurat)
-  require(liana)
-  require(lubridate)
-  
-  
-  # Define the functions needed to perform our analysis
-  
-  # Define the iterator wrapper function (wrap_resource_Iterator), which 
-  # produces the end results. The wrapper iterates the evaluator function and 
-  # collates its results.
-  source("Code/Resource_Dilution/RD_Iterator.R")
-  # Define general functions for data processing in the iterator
-  source("Code/Resource_Dilution/Iterator_Processing_Functions.R")
-  # Define functions for capturing metadata and saving iterator results
-  source("Code/Resource_Dilution/Iterator_Metadata_and_Saves.R")
-  
-  
-  # The evaluator function tests method robustness over multiple dilution stages
-  # The script below defines resource_Robustness, a function for testing 
-  # resource robustness
-  source("Code/Resource_Dilution/RD_Evaluator.R")
-  # Define functions that help the evaluator analyse top-ranked CCIs
-  source("Code/Resource_Dilution/Evaluator_Top_Ranks_Functions.R")
-  # Define functions that help the evaluator run LIANA
-  source("Code/Resource_Dilution/Evaluator_LIANA_Functions.R")
-  
-  
-  # Define the dilutor function, a function that dilutes resources.
-  source("Code/Resource_Dilution/RD_Dilutor.R")
-  # Define general helper functions for the dilutor 
-  source("Code/Resource_Dilution/Dilutor_Processing_Functions.R")
-  
-  
-  # Define utility functions that are used in resource dilution and in cluster
-  # reshuffling.
-  # Define common functions for the Iterators
-  source("Code/Utilities/Iterator_Functions.R")
-  # Define common functions for console outputs and plots 
-  source("Code/Utilities/User_Outputs_and_Plots.R")
-  # Define common functions that help with NATMI parameters
-  source("Code/Utilities/LIANA_Utilities.R")
-  
-  
-  
-  
+    # In this segment, we set up all the required infrastructure to run the
+    # wrapper. The default parameters for the wrapper are set in
+    # RD_Robustness_Iterator.R, but custom parameters can be set in the function
+    # call.
+
+    # Load required Packages
+    require(tidyverse)
+    require(Seurat)
+    require(liana)
+    require(lubridate)
+
+
+    # Define the functions needed to perform our analysis
+
+    # Define the iterator wrapper function (wrap_resource_Iterator), which
+    # produces the end results. The wrapper iterates the evaluator function and
+    # collates its results.
+    source("analysis/robustness/Code/Resource_Dilution/RD_Iterator.R")
+    # Define general functions for data processing in the iterator
+    source("analysis/robustness/Code/Resource_Dilution/Iterator_Processing_Functions.R")
+    # Define functions for capturing metadata and saving iterator results
+    source("analysis/robustness/Code/Resource_Dilution/Iterator_Metadata_and_Saves.R")
+
+
+    # The evaluator function tests method robustness over multiple dilution stages
+    # The script below defines resource_Robustness, a function for testing
+    # resource robustness
+    source("analysis/robustness/Code/Resource_Dilution/RD_Evaluator.R")
+    # Define functions that help the evaluator run LIANA
+    source("analysis/robustness/Code/Resource_Dilution/Evaluator_LIANA_Functions.R")
+
+
+    # Define the dilutor function, a function that dilutes resources.
+    source("analysis/robustness/Code/Resource_Dilution/RD_Dilutor.R")
+    # Define general helper functions for the dilutor
+    source("analysis/robustness/Code/Resource_Dilution/Dilutor_Processing_Functions.R")
+
+
+    # Define utility functions that are used in resource dilution and in cluster
+    # reshuffling.
+    # Define common functions for the Iterators
+    source("analysis/robustness/Code/Utilities/Iterator_Functions.R")
+    # Define common functions for console outputs and plots
+    source("analysis/robustness/Code/Utilities/User_Outputs_and_Plots.R")
+    # Define common functions that help with NATMI parameters
+    source("analysis/robustness/Code/Utilities/LIANA_Utilities.R")
+
+
+
+
 }
 
 
@@ -70,53 +68,44 @@
 # We get SLURM input arguments, including job_id to mark NATMI files with.
 args = commandArgs(TRUE)
 
-modify_baseline   = as.logical(args[1])
-feature_type      = as.character(args[2])
-top_n             = as.numeric(args[3])
-job_id            = as.numeric(args[4])
-preserve_topology = as.logical(args[5])
+modify_baseline <- as.logical(args[1])
+job_id          <- as.numeric(args[2])
 
 
-# First we load testdata from the data folder. 
+# First we load testdata from the data folder.
 # We also give a label (testdata_type, choose "seurat_pbmc" or "liana_test")
-testdata_type <- "seurat_pbmc"  
+testdata_type <- "seurat_pbmc"
 testdata      <- extract_Testdata(testdata_type = testdata_type)
 
 
 # Double-check the Inputs
 {
-  important_args <- 
-    c("testdata_type", 
+  important_args <-
+    c("testdata_type",
       "modify_baseline",
-      "feature_type", 
-      "top_n", 
-      "job_id",
-      "preserve_topology") 
-  
+      "job_id")
+
   map(important_args, function(arg) {
     c(arg, get(arg), typeof(get(arg)))
-    
-  }) %>% 
+
+  }) %>%
     transpose %>%
     set_names(c("name", "value", "data_type")) %>%
-    as_tibble() %>% 
+    as_tibble() %>%
     unnest(c("name", "value", "data_type")) %>%
     print()
-  
+
   cat("\n\nTestdata: \n")
   print(testdata)
 }
 
 
 # We run the wrapper with test settings
-robustness_resource <- 
+robustness_resource <-
   wrap_resource_Iterator(testdata      = testdata,
                          testdata_type = testdata_type,
-                         
+
                          modify_baseline   = modify_baseline,
-                         preserve_topology = preserve_topology,
-                         feature_type      = feature_type,
-                         top_n             = top_n,
                          NATMI_tag         = job_id)
 
 
