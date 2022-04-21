99aeaf8d21374c16bd81704c4c119e7339d0a060
dbdimitrov <d.boykoff@gmail.com>
2022-04-21 13:23:43 +0200

    add zenodo repo

--- analysis/initiate_environment.R
+++ analysis/initiate_environment.R
@@ -8,7 +8,10 @@ renv::restore()
 remotes::install_github("saezlab/liana", ref="b6d13c5")
 # https://github.com/saezlab/liana/tree/b6d13c555e943e6cbf650130b5abb91d7e7d1087
 
-# Install LIANA++ version following this tutorial:
+# Also deposited to Zenodo:
+# https://zenodo.org/record/6475164
+
+# Then make sure to install LIANA++ version following this tutorial:
 # https://saezlab.github.io/liana/articles/liana_devel.html
 
 # Obtain OmniPath /w mouse symbols and save
